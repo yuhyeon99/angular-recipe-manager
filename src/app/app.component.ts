698fd4ed695c09c8a48d20fc79323d255496e616
yuhyeon99 <jujini31@gmail.com>
2023-12-08 22:50:45 +0900

    독립형 server 컴포넌트 생성 및 import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ServerComponent } from './server/server.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ServerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-recipe-manager';
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    standalone: true,
+    templateUrl: './server.component.html',
+})
+export class ServerComponent {
+
+}
