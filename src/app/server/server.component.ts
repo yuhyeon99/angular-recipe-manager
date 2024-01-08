5effc1d2318457e6665835d563595568cec0ea6e
yuhyeon99 <jujini31@gmail.com>
2024-01-08 22:24:35 +0900

    문자열 보간 함수 사용

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,4 +7,8 @@ import { Component } from "@angular/core";
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'offLine';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
