11ab687176cc4fc29174345a5d8c0e3fc21a79c1
yuhyeon99 <jujini31@gmail.com>
2024-01-14 22:54:26 +0900

    이벤트 바인딩

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit{
   allowNewServer = false;
+  serverCreationStatus = 'No Server was created!'
 
   constructor() {
     setTimeout(()=> {
@@ -20,5 +21,9 @@ export class ServersComponent implements OnInit{
   }
   ngOnInit(): void {
     
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 }
