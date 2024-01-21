5fa737bfa2263926c012a50cd488248787665baf
yuhyeon99 <jujini31@gmail.com>
2024-01-21 22:38:53 +0900

    ngFor 사용

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit{
   serverCreationStatus = 'No Server was created!'
   serverName = 'TestServer';
   serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
   
   constructor() {
     setTimeout(()=> {
@@ -27,6 +28,7 @@ export class ServersComponent implements OnInit{
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
