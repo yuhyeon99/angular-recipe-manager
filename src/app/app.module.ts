fb9512cd7956dc3f5d66664cb34de1ea2e4e9ab6
yuhyeon99 <jujini31@gmail.com>
2024-01-24 23:25:11 +0900

    프로젝트 초기화 버전으로 복구

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    ServerComponent, ServersComponent,
+    AppComponent,
   ],
   imports: [
     BrowserModule,
