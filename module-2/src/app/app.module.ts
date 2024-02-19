65d460b4cc5336f3b7e595df01678839bf895525
kyheroumtech <jujini31@gmail.com>
2024-02-19 23:48:35 +0900

    컴포넌트 생성

--- module-2/src/app/app.module.ts
+++ module-2/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CockpitComponent
   ],
   imports: [
     BrowserModule,
