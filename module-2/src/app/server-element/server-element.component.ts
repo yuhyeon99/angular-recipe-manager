f6b36d02d042e0d9f67233c2f075a3d072ef2871
kyheroumtech <jujini31@gmail.com>
2024-02-28 23:34:26 +0900

    커스텀 속성 바인딩

--- module-2/src/app/server-element/server-element.component.ts
+++ module-2/src/app/server-element/server-element.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent {
-  
+export class ServerElementComponent implements OnInit {
+  element: {};
+
+  constructor() {}
+  ngOnInit(){
+
+  }
 }
