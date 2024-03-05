a541ee61bdb971f0c7cac9ee29220e59283ea484
yuhyeon99 <jujini31@gmail.com>
2024-03-05 23:21:10 +0900

    커스텀 이벤트 바인딩(With. @Output 데코레이션, EventEmitter)
    @Output 데코레이션 활용하

--- module-2/src/app/cockpit/cockpit.component.ts
+++ module-2/src/app/cockpit/cockpit.component.ts
@@ -1,27 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-cockpit',
-  templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  selector: "app-cockpit",
+  templateUrl: "./cockpit.component.html",
+  styleUrls: ["./cockpit.component.css"],
 })
 export class CockpitComponent {
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  @Output() bluePrintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+
   serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  newServerName = "";
+  newServerContent = "";
   onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent,
     });
   }
 
   onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+    this.bluePrintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent,
     });
   }
 }
