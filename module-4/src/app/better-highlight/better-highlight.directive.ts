ad4a33a52bdd953658dcebbb69043243483513bf
yuhyeon99 <jujini31@gmail.com>
2024-05-01 23:09:08 +0900

    디렉티브에 HostListener 등록

--- module-4/src/app/better-highlight/better-highlight.directive.ts
+++ module-4/src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,19 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
-
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue', false, false);
+  }
+  @HostListener('mouseenter') mouseover(eventData: Event){
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
   }
 }
