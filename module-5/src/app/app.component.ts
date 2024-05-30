cba15913fa5a92a37b9d85bec1e82804eaf5d476
yuhyeon99 <jujini31@gmail.com>
2024-05-30 23:07:37 +0900

    앱 컴포넌트 providers에서 서비스 제거
    앱 모듈에서 providers에서 공급해줌
    중복 공급할 경우 에러 발생

--- module-5/src/app/app.component.ts
+++ module-5/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { AccountsService } from './account.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AccountsService]
 })
 export class AppComponent implements OnInit{
   accounts: {name: string, status: string}[] = [];
