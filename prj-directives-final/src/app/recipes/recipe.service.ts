3932d870f1159635d7e2ebc0c33167154ee5695a
yuhyeon99 <jujini31@gmail.com>
2024-06-03 23:11:51 +0900

    레시피 정보를 반환하는 getter 함수 추가

--- prj-directives-final/src/app/recipes/recipe.service.ts
+++ prj-directives-final/src/app/recipes/recipe.service.ts
@@ -5,4 +5,8 @@ export class RecipeService {
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
     new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
+  
+  getRecipes() {
+    return this.recipes;
+  }
 }
