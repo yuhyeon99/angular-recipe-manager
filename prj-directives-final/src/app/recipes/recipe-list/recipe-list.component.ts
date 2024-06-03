71d7533d8c8e0bf2fc2997aa74aaa5b588895683
yuhyeon99 <jujini31@gmail.com>
2024-06-03 23:15:31 +0900

    레시피 서비스 초기화 이후 레시피 할당

--- prj-directives-final/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-directives-final/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,10 +10,13 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-
-  constructor() { }
+  recipes: Recipe[];
+  constructor(
+    public recipeService: RecipeService
+  ) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
